40134fbe18d3e916ddfc1bd284c146bdf94efe6f
obaidmuneer <obaidmuneer55@gmail.com>
2024-01-02 17:30:54 +0500

    intro to javascript

---
+++ class 05/app.mjs
@@ -0,0 +1,8 @@
+console.log("Hello Javascript");
+console.log("1");
+// alert("Hello world");
+console.log(2);
+console.log("three");
+// document.write("Hello Js")
+document.querySelector("#header").innerHTML = "<div><h1 class='bg-red-300' >Hello Header</h1></div>"
+
