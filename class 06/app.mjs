b028f2102522de5b1a2c68acabde6c8d6a0135ad
obaidmuneer <obaidmuneer55@gmail.com>
2024-01-03 19:18:57 +0500

    class 06

---
+++ class 06/app.mjs
@@ -0,0 +1,36 @@
+// document.querySelector("#container").innerHTML = `<h1>
+// 123 <br>
+// 4567<br>
+// 789<br>
+// </h1>`
+// document.querySelector(".sub").innerHTML = "Hello world"
+
+// document.querySelector(".counter").innerHTML = ""
+
+// name = "Hello world!"
+// var name = "Hello world!"
+// var name = "Hello world!"
+// var name = "Hello world!"
+// let name = "Hello Obaid"
+// // name = "Hello Hammad"
+// const name = "Hello Hussain"
+// // name = "Hello shoaib"
+
+// console.log(name);
+
+let name = "Hasan"; // string
+let welcome = "Hello ";
+let age = 20; // number
+let isMarried = true; // boolean
+let isChild = false; // boolean
+
+console.log(name);
+console.log(age);
+console.log(isMarried);
+console.log(isChild);
+console.log(undefined); // undefined
+console.log(null); // Null
+
+// console.log(welcome+ " " +name);
+console.log(welcome + name + " and your age is " + age);
+// document.querySelector(".sub").innerHTML = "Hello world"
